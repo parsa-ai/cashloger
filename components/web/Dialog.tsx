2967251f754b53d4e95f7cd54e80a13401f8864c
parsa <parsaalavi9@gmail.com>
2026-02-23 18:55:36 +0330

    make dialog better

--- components/web/Dialog.tsx
+++ components/web/Dialog.tsx
@@ -23,7 +23,8 @@ import {
 } from "@/components/ui/select"
 import { chartConfig } from "@/lib/chartConfigs"
 import { useChartStore } from "@/store/useChartStore"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
+import { ButtonGroup } from "../ui/button-group"
 
 export function DialogBox() {
     type ChartKey = Exclude<keyof typeof chartConfig, "visitors">
@@ -34,30 +35,42 @@ export function DialogBox() {
 
     const [category, setCategory] = useState(firstKey)
     const [amount, setAmount] = useState("")
-    const increase = useChartStore((state) => state.increase)
+    const [open, setOpen] = useState(false)
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const chartData = useChartStore((state) => state.chartData)
+    const increase = useChartStore((state) => state.increase)
+    const decrease = useChartStore((state) => state.decrease)
+
+    const handleSubmit = (e: FormEvent) => {
         e.preventDefault()
-        console.log(amount, category);
         const numericAmount = Number(amount)
         if (!numericAmount || numericAmount <= 0) return
         increase(category, numericAmount)
         setAmount("")
+        setOpen(false)
+    }
+    const handleReset = (e: FormEvent) => {
+        e.preventDefault()
+        const numericAmount = Number(amount)
+        if (!numericAmount || numericAmount <= 0) return
+        decrease(category, numericAmount)
+        setAmount("")
+        setOpen(false)
     }
     return (
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <Button variant="outline">ادیت خرج ها</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-sm">
-                <form onSubmit={handleSubmit}>
-                    <DialogHeader>
+                <form onSubmit={handleSubmit} onReset={handleReset}>
+                    <DialogHeader className="my-4 items-center">
                         <DialogTitle>ادیت خرج ها</DialogTitle>
                         <DialogDescription>
                             {"اینجا میتونی خرج یا ورودی جدیدت رو اضافه کنی"}
                         </DialogDescription>
                     </DialogHeader>
-                    <FieldGroup className="gap-4">
+                    <FieldGroup className="gap-5">
                         <Field>
                             <Label htmlFor="amount">مقدار</Label>
                             <Input id="amount" name="amount" type="number" placeholder="۱۰۰۰ تومان"
@@ -74,22 +87,24 @@ export function DialogBox() {
                                 <SelectContent position="popper" side="bottom"
                                     avoidCollisions={false}>
                                     <SelectGroup >
-                                        {
-                                            items.map(([key, value]) => (
-                                                <SelectItem key={key} value={key}>
-                                                    {value.label}
-                                                </SelectItem>
-                                            ))}
+                                        {items.map(([key, value]) => (
+                                            <SelectItem key={key} value={key}>
+                                                {value.label} {chartData.find((item) => item.browser === key)?.visitors ? `(${chartData.find((item) => item.browser === key)?.visitors})` : ""}
+                                            </SelectItem>
+                                        ))}
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
                         </Field>
                     </FieldGroup>
-                    <DialogFooter className="mt-4">
+                    <DialogFooter className="mt-7 justify-between sm:justify-between">
+                        <ButtonGroup dir="ltr" >
+                            <Button type="reset" variant="outline">حذف</Button>
+                            <Button type="submit" variant="default">افزودن</Button>
+                        </ButtonGroup>
                         <DialogClose asChild>
                             <Button variant="outline">بیخیال</Button>
                         </DialogClose>
-                        <Button type="submit">ذخیره تغییرات</Button>
                     </DialogFooter>
                 </form>
             </DialogContent>
