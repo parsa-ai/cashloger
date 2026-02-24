33b93a7c966cb81996b02ef4aa42dd5db06b15f8
parsa <parsaalavi9@gmail.com>
2026-02-24 13:25:07 +0330

    add go to today

--- components/web/DateC.tsx
+++ components/web/DateC.tsx
@@ -16,33 +16,41 @@ export function DateC() {
   const [open, setOpen] = useState(false)
   const date = useDateStore((s) => s.date) || new Date()
   const setDate = useDateStore((s) => s.setDate)
+  const isToday = date.toDateString() === new Date().toDateString();
+  console.log(date.toDateString(), new Date().toDateString());
+
   return (
-    <Field className="w-full">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            id="date"
-            className="justify-center font-normal"
-          >
-            {date ? date.toLocaleDateString("FA-IR") : "انتخاب کن"}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto overflow-hidden p-0" align="center">
-          <Calendar
-            mode="single"
-            selected={date}
-            defaultMonth={date}
-            captionLayout="dropdown"
-            onSelect={(date) => {
-              if (date) {
-                setDate(date)
-                setOpen(false)
-              }
-            }}
-          />
-        </PopoverContent>
-      </Popover>
-    </Field>
+    <div className="flex items-center gap-2">
+      <Button variant="ghost" onClick={() => setDate(new Date())} hidden={isToday}>
+        برو به امروز
+      </Button>
+      <Field className="w-full">
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date"
+              className="justify-center font-normal"
+            >
+              {date ? date.toLocaleDateString("FA-IR") : "انتخاب کن"}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto overflow-hidden p-0" align="center">
+            <Calendar
+              mode="single"
+              selected={date}
+              defaultMonth={date}
+              captionLayout="dropdown"
+              onSelect={(date) => {
+                if (date) {
+                  setDate(date)
+                  setOpen(false)
+                }
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </Field>
+    </div>
   )
 }
